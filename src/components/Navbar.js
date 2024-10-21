54e743fd24cac7e10e14f118c478b49193d78055
sreeraj <sreerajmutha@gmail.com>
2024-10-21 16:51:17 +0530

    Added menu bar and made navbar as responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../stylesSheets/Navbar.css';
 
 export default function NavBar() {
-    
-    const navigate = useNavigate(); 
+    const [menuOpen, setMenuOpen] = useState(false);
+    const navigate = useNavigate();
 
     function Move() {
-        navigate('/login'); 
+        navigate('/login');
+    }
+
+    function toggleMenu() {
+        setMenuOpen(!menuOpen); 
     }
 
     return (
         <>
-            <nav className='nav'>
-                <h3 className='logo'>LOGO</h3>
-                <ul className='ul'>
-                    <li className='li'>Home</li>
-                    <li className='li'>Recipe</li>
-                    <li className='li'>About us</li> 
+            <nav className="nav">
+                <h3 className="logo">LOGO</h3>
+                <ul className={`ul ${menuOpen ? 'open' : ''}`}>
+                    <li className="li">Home</li>
+                    <li className="li">Recipe</li>
+                    <li className="li">About us</li>
                 </ul>
-                <button className='btn' onClick={Move}>Login</button>
+                <button className="btn" onClick={Move}>Login</button>
+                <div className="menu-icon" onClick={toggleMenu}>
+                    <i className={menuOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
             </nav>
         </>
     );
